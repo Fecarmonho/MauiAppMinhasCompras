e295a124f70930ff6f717a9023c235c09a7992a2
Fecarmonho <fe.carmonho@gmail.com>
2025-03-07 00:05:11 -0300

    Criar insert

--- MauiAppMinhasCompras-master/MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras-master/MauiAppMinhasCompras/Models/Produto.cs
@@ -6,9 +6,8 @@ namespace MauiAppMinhasCompras.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricão { get; set; }
-        public double Quantidade {  get; set; }
-        public double Preco {  get; set; }
-
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
